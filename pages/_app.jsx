48ebd7ce0c6381210f5dd3d40979c1d2ba551954
Dhiraj <dhiraj.basavaraju@gmail.com>
2023-11-17 07:47:14 +0530

    Update _app.jsx with clarity

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -23,6 +23,14 @@ export default function App({ Component, pageProps }) {
           gtag('config', 'G-JW6FHR00VP');
         `}
       </Script>
+      {/* Clarity */}
+      <Script type="text/javascript">
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "fq1vxacep7");`}
+      </Script>
       <main className={raleway.className}>
         <Component {...pageProps} flag={showResume} />
       </main>
